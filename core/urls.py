fb960742228b52f3eceb19f808dd6654ea597e85
Pablo Fernandez <pablo.fernandez@mercadolibre.com>
2017-05-03 17:02:08 -0300

    Added basic view to recieve mp notifications

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'core.views.home', name="home"),
     url(r'^product/([0-9]*)$', 'core.views.product', name="product"),
+    url(r'^mp-notifications$', 'core.views.mp_notifications', name="mp-notifications"),
 )
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from core.models import Product, Order
+from django.http import JsonResponse
 import mercadopago
 
 
@@ -38,3 +39,29 @@ def product(request, product_id):
         'product': product,
     }
     return render(request, 'core/product.html', context)
+
+
+def mp_notifications(request, **kwargs):
+    mp = mercadopago.MP(settings.MP_CLIENT_ID, settings.MP_CLIENT_SECRET)
+    
+    topic = request.GET['topic']
+    mp_id = request.GET['id']
+
+    merchant_order_info = None
+
+    if topic == "payment":
+        payment_info = mp.get("/collections/notifications/"+ mp_id)
+        if not payment_info["status"] == 200:
+            return JsonResponse({'status': payment_info["status"]})
+        merchant_order_info = mp.get("/merchant_orders/" + payment_info["response"]["collection"]["merchant_order_id"])
+    elif topic == "merchant_order":
+        merchant_order_info = mp.get("/merchant_orders/" + mp_id)
+
+    if merchant_order_info == None:
+        raise ValueError("Error obtaining the merchant_order")
+
+    if merchant_order_info["status"] == 200:
+        return JsonResponse({
+            "payment": merchant_order_info["response"]["payments"],
+            "shipment": merchant_order_info["response"]["shipments"]
+        })
