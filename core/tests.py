1dd607f9b1322337719f943d99befc42048c5f22
Pablo Fernandez <pablo.fernandez@mercadolibre.com>
2017-05-05 13:50:35 -0300

    Added testing to home view

--- core/tests.py
+++ core/tests.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from core.models import Order, Product, InvalidStateException
 from django.core.urlresolvers import reverse
@@ -40,28 +40,38 @@ class UrlsTestCase(TestCase):
 
     def test_login_url(self):
         url = reverse('login')
-        self.assertEquals(url, '/ingresar/')
+        self.assertEquals(url, '/ingresar')
 
     def test_logout_url(self):
         url = reverse('logout')
-        self.assertEquals(url, '/salir/')
+        self.assertEquals(url, '/salir')
 
     def test_orders_url(self):
         url = reverse('orders')
-        self.assertEquals(url, '/ordenes/')
+        self.assertEquals(url, '/ordenes')
 
     def test_purchase_success_url(self):
         url = reverse('mp-back-success')
-        self.assertEquals(url, '/compra-exitosa/')
+        self.assertEquals(url, '/compra-exitosa')
 
     def test_purchase_failure_url(self):
         url = reverse('mp-back-failure')
-        self.assertEquals(url, '/compra-fallida/')
+        self.assertEquals(url, '/compra-fallida')
 
     def test_purchase_pending_url(self):
         url = reverse('mp-back-pending')
-        self.assertEquals(url, '/compra-pago-pendiente/')
+        self.assertEquals(url, '/compra-pago-pendiente')
 
     def test_mp_notifications_url(self):
         url = reverse('mp-notifications')
-        self.assertEquals(url, '/mp-notifications/')
+        self.assertEquals(url, '/mp-notifications')
+
+
+class ViewTestCase(TestCase):
+
+    def test_home(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, 'MusicStore')
+        self.assertContains(response, u'Ingresá')
+        self.assertContains(response, u'Elegí tu producto')
